f049e63bf678034421881bb8e5e6b7ee4be367c2
Ivan Bertholdo <ivanbertholdo38@gmail.com>
2024-08-23 23:39:40 -0300

    aa

--- api/app.js
+++ api/app.js
@@ -17,12 +17,7 @@ const app = Fastify({
 })
 const PORT = process.env.PORT || 3333
 
-app.register(cors, {
-  origin: true, // Permite todas as origens
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
-});
-
+app.register(cors);
 app.register(fastifyJwt, {
   secret: strongPassword
 })
